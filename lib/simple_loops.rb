505c24f6867f0b8a2ed2b8bce02c54e6f3b70748
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-17 18:24:18 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,7 +22,7 @@ def return_string_array(array)
   count = 0
   new_array = Array.new
   while count < array.length do
-    array[count].to_s
+    new_array << array[count].to_s
     count += 1
   end
 end
