62f59de54a14daba89e864de059ae78c4ba97430
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-17 18:10:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+def loop_message_n_times(message, number)
+  number.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  puts array
+end
